9a1568e7aeffb3b56d9daa4c98dd893a5703ddf5
Glenn Ferrie <glenn.ferrie@gmail.com>
2012-12-23 01:05:31 -0500

    More Styling and Content
    ... also added first part for auth Fx

---
+++ StudentConnect/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentConnect.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult Index()
+        {
+            return RedirectToAction("Login");
+        }
+        
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
