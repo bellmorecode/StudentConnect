d6bbb0193f77ba109f8a3325561ee5e8a924bba6
Glenn Ferrie <glenn.ferrie@gmail.com>
2012-12-26 21:46:44 -0500

    switch to mluti-page, see notes
    implemented dynamic positions of form, switched to multi-page

--- StudentConnect/Controllers/HomeController.cs
+++ StudentConnect/Controllers/HomeController.cs
@@ -3,17 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Dynamic;
+using StudentConnect.Data;
 
 namespace StudentConnect.Controllers
 {
+    using Model;
     public class HomeController : Controller
     {
-        //
-        // GET: /Home/
+        IContentRepository repo;
 
+        public HomeController()
+        {
+            // TODO: Ioc should be here.
+            repo = new MockContentRepository();
+        }
+        
         public ActionResult Index()
         {
-            return View();
+            int index = 1;
+            var model = (from x in repo.GetPositions() select new PositionItem { Index = index++, Value = x.Title }).ToArray();
+            return View(model);
         }
 
     }
---
+++ StudentConnect/Controllers/Model/PositionItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StudentConnect.Controllers.Model
+{
+    public sealed class PositionItem
+    {
+        public int Index { get; set; }
+        public string Value { get; set; }
+    }
+}
