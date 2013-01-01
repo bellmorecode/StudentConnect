47de2fcae80824aaeca93bd133f0865d0236c826
Glenn Ferrie <glenn.ferrie@gmail.com>
2013-01-01 16:00:14 -0500

    fixed bug in the Content repo

--- StudentConnect.Core/Data/ContentRepository.cs
+++ StudentConnect.Core/Data/ContentRepository.cs
@@ -18,24 +18,48 @@ namespace StudentConnect.Data
         public AboutContent GetAbout()
         {
             var school = (SchoolData)HttpContext.Current.Session["_ActiveSchool"];
-            var data = store.GetSchoolMetadata(school.Alias);
-            if (data == null) data = store.GetSchoolMetadata("_Default");
+            SchoolMetadata data;
+            if (school == null)
+            {
+                data = store.GetSchoolMetadata("_Default");
+            }
+            else
+            {
+                data = store.GetSchoolMetadata(school.Alias);
+                if (data == null) data = store.GetSchoolMetadata("_Default");
+            }
             return data.About;
         }
 
         public IEnumerable<Person> GetPeople()
         {
             var school = (SchoolData)HttpContext.Current.Session["_ActiveSchool"];
-            var data = store.GetSchoolMetadata(school.Alias);
-            if (data == null) data = store.GetSchoolMetadata("_Default");
+            SchoolMetadata data;
+            if (school == null)
+            {
+                data = store.GetSchoolMetadata("_Default");
+            }
+            else
+            {
+                data = store.GetSchoolMetadata(school.Alias);
+                if (data == null) data = store.GetSchoolMetadata("_Default");
+            }
             return data.People;
         }
 
         public IEnumerable<Position> GetPositions()
         {
             var school = (SchoolData)HttpContext.Current.Session["_ActiveSchool"];
-            var data = store.GetSchoolMetadata(school.Alias);
-            if (data == null) data = store.GetSchoolMetadata("_Default");
+            SchoolMetadata data;
+            if (school == null)
+            {
+                data = store.GetSchoolMetadata("_Default");
+            }
+            else
+            {
+                data = store.GetSchoolMetadata(school.Alias);
+                if (data == null) data = store.GetSchoolMetadata("_Default");
+            } 
             return data.Positions;
         }
 
